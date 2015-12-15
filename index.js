63c671df509a54a123312cd3986bd4195ba46998
Vikas Sahu <vikassahu2904@gmail.com>
2015-12-15 11:29:20 +0530

    Initial socket.io express setup

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+server.listen(8000, function() {
+  console.log('Server started');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket) {
+  socket.emit('joined', {message: 'New User Joined'});
+  socket.on('message', function(data) {
+    console.log(data);
+  });
+});
